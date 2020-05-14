b248929952d308281e04b027b3469b440dab522e
michael tamsil <michaeltamsil@gmail.com>
2020-05-14 15:20:02 +0700

    try use UIViewStack

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,11 +3,9 @@
 import UIKit
 import PlaygroundSupport
 
-
-
 class MyViewController : UIViewController {
-
-
+    var mainStackView = UIStackView()
+    
     var stackViewBtn = UIStackView()
     var stackViewBtn2 = UIStackView()
     let stackViewAllBtn = UIStackView()
@@ -25,50 +23,52 @@ class MyViewController : UIViewController {
     
     override func loadView() {
         let view = UIView()
-        let label = UILabel()
-        let number1 = UILabel()
-        let operator_ = UILabel()
-        let number2 = UILabel()
-        let inputValue = UITextField()
-        let assignment = UILabel()
+        let title = UILabel()
+//        let number1 = UILabel()
+//        let operator_ = UILabel()
+//        let number2 = UILabel()
+//        let inputValue = UITextField()
+//        let assignment = UILabel()
+        
+        setStackView(stackView: &mainStackView)
         
         view.backgroundColor = .orange
 
-        label.frame = CGRect(x: 10, y: 10, width: 300, height: 30)
-        label.text = "Test my math skill"
-        label.textColor = .black
-        label.font = label.font.withSize(32)
-        label.backgroundColor = .red
-        view.addSubview(label)
+        title.frame = CGRect(x: 10, y: 10, width: 300, height: 30)
+        title.text = "Test my math skill"
+        title.textColor = .black
+        title.font = title.font.withSize(32)
+        title.backgroundColor = .red
+        view.addSubview(title)
         
-        number1.frame = CGRect(x:20, y: 50, width: 80, height: 80)
-        number1.text = "99"
-        number1.textColor = .black
-        number1.backgroundColor = .blue
-        number1.font = UILabel().font.withSize(64)
-        view.addSubview(number1)
+//        number1.frame = CGRect(x:20, y: 50, width: 80, height: 80)
+//        number1.text = "99"
+//        number1.textColor = .black
+//        number1.backgroundColor = .blue
+//        number1.font = UILabel().font.withSize(64)
+//        view.addSubview(number1)
+//
+//        operator_.frame = CGRect(x:110, y: 50, width: 40, height: 80)
+//        operator_.text = "+"
+//        operator_.textColor = .black
+//        operator_.backgroundColor = .green
+//        operator_.font = UILabel().font.withSize(64)
+//        view.addSubview(operator_)
+//
+//        number2.font = UILabel().font.withSize(64)
+//        number2.frame = CGRect(x:160, y: 50, width: 80, height: 80)
+//        number2.text = "99"
+//        number2.textColor = .black
+//        number2.backgroundColor = .blue
+//        view.addSubview(number2)
         
-        operator_.frame = CGRect(x:110, y: 50, width: 40, height: 80)
-        operator_.text = "+"
-        operator_.textColor = .black
-        operator_.backgroundColor = .green
-        operator_.font = UILabel().font.withSize(64)
-        view.addSubview(operator_)
         
-        number2.font = UILabel().font.withSize(64)
-        number2.frame = CGRect(x:160, y: 50, width: 80, height: 80)
-        number2.text = "99"
-        number2.textColor = .black
-        number2.backgroundColor = .blue
-        view.addSubview(number2)
-        
-        
-        assignment.frame = CGRect(x:250, y: 50, width: 40, height: 80)
-        assignment.text = "="
-        assignment.textColor = .black
-        assignment.backgroundColor = .green
-        assignment.font = UILabel().font.withSize(64)
-        view.addSubview(assignment)
+//        assignment.frame = CGRect(x:250, y: 50, width: 40, height: 80)
+//        assignment.text = "="
+//        assignment.textColor = .black
+//        assignment.backgroundColor = .green
+//        assignment.font = UILabel().font.withSize(64)
+//        view.addSubview(assignment)
         
         
         inputValue.isEnabled = false
@@ -94,17 +94,28 @@ class MyViewController : UIViewController {
         stackViewBtn2.addArrangedSubview(button6)
         stackViewBtn2.backgroundColor = .purple
 //        view.addSubview(stackViewBtn2)
-        
+        stackViewAllBtn.axis = .vertical
         stackViewAllBtn.alignment = .fill
-        stackViewAllBtn.distribution = .fill
-        stackViewAllBtn.spacing = 20
+        stackViewAllBtn.distribution = .fillEqually
+        stackViewAllBtn.spacing = 30
         stackViewAllBtn.translatesAutoresizingMaskIntoConstraints = false
         stackViewAllBtn.addArrangedSubview(stackViewBtn)
         stackViewAllBtn.addArrangedSubview(stackViewBtn2)
-    
-        view.addSubview(stackViewAllBtn)
         
-        self.view = view
+        
+        
+        
+        //view.addSubview(stackViewAllBtn)
+        mainStackView.addArrangedSubview(title)
+        
+        mainStackView.addArrangedSubview(stackViewBtn)
+        mainStackView.addArrangedSubview(stackViewBtn2)
+        self.view = mainStackView
+    }
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        setStackViewConstraints(stackView : &mainStackView)
     }
 }
 
@@ -124,9 +135,27 @@ func createButton(message: String) -> UIButton{
     button.layer.cornerRadius = 5
     button.layer.borderWidth = 1
     button.layer.borderColor = UIColor.black.cgColor
+    
     return button;
 }
+
+func setStackView(stackView: inout UIStackView) {
+    stackView.axis = .vertical
+    stackView.alignment = .fill
+    stackView.distribution = .fillEqually
+}
+
+func setStackViewConstraints(stackView: inout UIStackView) {
+    print("halo 3")
+    stackView.translatesAutoresizingMaskIntoConstraints
+    stackView.topAnchor.constraint(equalTo: viewController.view.bottomAnchor, constant: 20).isActive
+    stackView.leadingAnchor.constraint(equalTo: viewController.view.leadingAnchor, constant: 50).isActive
+    stackView.trailingAnchor.constraint(equalTo: viewController.view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive
+    stackView.bottomAnchor.constraint(equalTo: viewController.view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive
+}
+
 // Present the view controller in the Live View window
 let viewController = MyViewController()
+
 PlaygroundPage.current.liveView = viewController
 
