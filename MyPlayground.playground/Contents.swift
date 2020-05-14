f66657309fd91df65523b51ccdac1e9eebb2e607
michael tamsil <michaeltamsil@gmail.com>
2020-05-15 00:50:13 +0700

    using stackview

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,12 +3,16 @@
 import UIKit
 import PlaygroundSupport
 
+var title = UILabel()
+
 class MyViewController : UIViewController {
     var mainStackView = UIStackView()
     
     var stackViewBtn = UIStackView()
     var stackViewBtn2 = UIStackView()
+    var stackViewBtn3 = UIStackView()
     let stackViewAllBtn = UIStackView()
+    
     let button1 = createButton(message: "1")
     let button2 = createButton(message: "2")
     let button3 = createButton(message: "3")
@@ -20,96 +24,52 @@ class MyViewController : UIViewController {
     let button9 = createButton(message: "9")
     let button0 = createButton(message: "0")
     let buttonDot = createButton(message: ".")
+    let buttonClear = createButton(message: "clear")
+    let header = funcHeader()
     
     override func loadView() {
-        let view = UIView()
-        let title = UILabel()
-//        let number1 = UILabel()
-//        let operator_ = UILabel()
-//        let number2 = UILabel()
-//        let inputValue = UITextField()
-//        let assignment = UILabel()
+        
+        let question = funcQuestion()
+        let answer = funcAnswer()
+        //let mathQuestion = UILabel()
         
         setStackView(stackView: &mainStackView)
-        
-        view.backgroundColor = .orange
-
-        title.frame = CGRect(x: 10, y: 10, width: 300, height: 30)
-        title.text = "Test my math skill"
-        title.textColor = .black
-        title.font = title.font.withSize(32)
-        title.backgroundColor = .red
-        view.addSubview(title)
-        
-//        number1.frame = CGRect(x:20, y: 50, width: 80, height: 80)
-//        number1.text = "99"
-//        number1.textColor = .black
-//        number1.backgroundColor = .blue
-//        number1.font = UILabel().font.withSize(64)
-//        view.addSubview(number1)
-//
-//        operator_.frame = CGRect(x:110, y: 50, width: 40, height: 80)
-//        operator_.text = "+"
-//        operator_.textColor = .black
-//        operator_.backgroundColor = .green
-//        operator_.font = UILabel().font.withSize(64)
-//        view.addSubview(operator_)
-//
-//        number2.font = UILabel().font.withSize(64)
-//        number2.frame = CGRect(x:160, y: 50, width: 80, height: 80)
-//        number2.text = "99"
-//        number2.textColor = .black
-//        number2.backgroundColor = .blue
-//        view.addSubview(number2)
-        
-        
-//        assignment.frame = CGRect(x:250, y: 50, width: 40, height: 80)
-//        assignment.text = "="
-//        assignment.textColor = .black
-//        assignment.backgroundColor = .green
-//        assignment.font = UILabel().font.withSize(64)
-//        view.addSubview(assignment)
-        
-        
-//        inputValue.isEnabled = false
-//        inputValue.text="2938"
-//        inputValue.backgroundColor = .red
-//        inputValue.textColor = .blue
-//        inputValue.frame = CGRect(x:10, y: 150, width: 260, height: 100)
-//        inputValue.font = UIFont.systemFont(ofSize:100)
-//        view.addSubview(inputValue)
         
         setViewButton(uIStackView: &stackViewBtn)
         stackViewBtn.addArrangedSubview(button1)
         stackViewBtn.addArrangedSubview(button2)
         stackViewBtn.addArrangedSubview(button3)
-     
-        
-        stackViewBtn.backgroundColor = .purple
-//        view.addSubview(stackViewBtn)
+        stackViewBtn.addArrangedSubview(button4)
         
         setViewButton(uIStackView: &stackViewBtn2)
-        stackViewBtn2.addArrangedSubview(button4)
         stackViewBtn2.addArrangedSubview(button5)
         stackViewBtn2.addArrangedSubview(button6)
-        stackViewBtn2.backgroundColor = .purple
-//        view.addSubview(stackViewBtn2)
+        stackViewBtn2.addArrangedSubview(button7)
+        stackViewBtn2.addArrangedSubview(button8)
+        
+        setViewButton(uIStackView: &stackViewBtn3)
+        stackViewBtn3.addArrangedSubview(button9)
+        stackViewBtn3.addArrangedSubview(button0)
+        stackViewBtn3.addArrangedSubview(buttonDot)
+        stackViewBtn3.addArrangedSubview(buttonClear)
+        
+        stackViewAllBtn.backgroundColor = .red
         stackViewAllBtn.axis = .vertical
         stackViewAllBtn.alignment = .fill
         stackViewAllBtn.distribution = .fillEqually
-        stackViewAllBtn.spacing = 30
+        stackViewAllBtn.spacing = 1
         stackViewAllBtn.translatesAutoresizingMaskIntoConstraints = false
         stackViewAllBtn.addArrangedSubview(stackViewBtn)
         stackViewAllBtn.addArrangedSubview(stackViewBtn2)
+        stackViewAllBtn.addArrangedSubview(stackViewBtn3)
+        
+        mainStackView.addArrangedSubview(header)
+        
+        mainStackView.addArrangedSubview(question)
+        mainStackView.addArrangedSubview(answer)
+        mainStackView.addArrangedSubview(stackViewAllBtn)
         
         
-        
-        
-        //view.addSubview(stackViewAllBtn)
-        mainStackView.addArrangedSubview(title)
-        
-        mainStackView.addArrangedSubview(stackViewBtn)
-        mainStackView.addArrangedSubview(stackViewBtn2)
         self.view = mainStackView
     }
     
@@ -122,10 +82,10 @@ class MyViewController : UIViewController {
 
 func setViewButton(uIStackView: inout UIStackView){
     uIStackView.axis = .horizontal
-    uIStackView.alignment = .fill
-    uIStackView.distribution = .fill
+    uIStackView.alignment = .firstBaseline
+    uIStackView.distribution = .fillEqually
     uIStackView.spacing = 10
-    uIStackView.translatesAutoresizingMaskIntoConstraints = false
+    //uIStackView.translatesAutoresizingMaskIntoConstraints = false
 }
 
 func createButton(message: String) -> UIButton{
@@ -146,13 +106,59 @@ func setStackView(stackView: inout UIStackView) {
 }
 
 func setStackViewConstraints(stackView: inout UIStackView) {
-    print("halo 3")
     stackView.translatesAutoresizingMaskIntoConstraints
     stackView.topAnchor.constraint(equalTo: viewController.view.bottomAnchor, constant: 20).isActive
     stackView.leadingAnchor.constraint(equalTo: viewController.view.leadingAnchor, constant: 50).isActive
     stackView.trailingAnchor.constraint(equalTo: viewController.view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive
     stackView.bottomAnchor.constraint(equalTo: viewController.view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive
 }
+
+func funcHeader() -> UIView{
+    let view = UIView()
+    
+    view.backgroundColor = .blue
+    view.sizeToFit()
+    
+    
+    
+    title.text = "Test my math skill"
+    title.textColor = .black
+    title.font = title.font.withSize(42)
+    title.backgroundColor = .red
+    title.sizeToFit()
+    
+    //view.height = 1
+    view.addSubview(title)
+    
+    return view
+}
+func funcQuestion() -> UIView {
+    let view = UIView()
+    view.backgroundColor = .cyan
+    let title = UILabel()
+    title.text = "90 x 43"
+    title.textColor = .black
+    title.font = title.font.withSize(64)
+    title.backgroundColor = .red
+    title.sizeToFit()
+    view.addSubview(title)
+    return view
+}
+
+func funcAnswer() -> UIView {
+    let view = UIView()
+    view.backgroundColor = .green
+    
+    let answer = UILabel()
+    answer.text = "xxx"
+    answer.textColor = .black
+    answer.backgroundColor = .red
+    answer.font = answer.font.withSize(64)
+    answer.sizeToFit()
+    view.addSubview(answer)
+    return view
+}
+
 
 // Present the view controller in the Live View window
 let viewController = MyViewController()
